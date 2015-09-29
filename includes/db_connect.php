82c4aac52310fe0f9de34d0c46bb474e1aada676
Sam Turner <sam.turner@students.mq.edu.au>
2015-09-29 21:51:23 +1000

    delete wrong files

--- includes/db_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once 'psl-config.php';   // As functions2.php is not included
-//$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-$conn = new mysqli(HOST, USER, PASSWORD, DATABASE);
-//$conn = oci_connect($oraUser,$oraPass,$oraDB);
--- js/fieldCookieSet.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-
-  var today = new Date();
-  var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
-
-  function setCookie(name, value)
-  {
-    document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
-  }
