6f0c8eed81ed8de8dde3becd730cdf63cb92c2e0
Jacob Williams <jacob_luke500@hotmail.com>
2015-10-16 09:28:36 +1100

    Login bug fixed
    Username parameter was binding to integer in SQL prepared statement.
    Changed to bind to string

--- includes/functions2.php
+++ includes/functions2.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'psl-config.php';
- 
+
 function sec_session_start() {
     $session_name = 'sec_session_id';   // Set a custom session name
     $secure = SECURE;
@@ -14,19 +14,19 @@ function sec_session_start() {
     // Gets current cookies params.
     $cookieParams = session_get_cookie_params();
     session_set_cookie_params($cookieParams["lifetime"],
-        $cookieParams["path"], 
-        $cookieParams["domain"], 
+        $cookieParams["path"],
+        $cookieParams["domain"],
         $secure,
         $httponly);
     // Sets the session name to the one set above.
     session_name($session_name);
-    session_start();            // Start the PHP session 
-    session_regenerate_id(true);    // regenerated the session, delete the old one. 
+    session_start();            // Start the PHP session
+    session_regenerate_id(true);    // regenerated the session, delete the old one.
 }
 
 
 function login($email, $password, $mysqli) {
-    // Using prepared statements means that SQL injection is not possible. 
+    // Using prepared statements means that SQL injection is not possible.
     if ($stmt = $mysqli->prepare("SELECT members.userid, members.email, members.username, members.fname, members.lname, members.sClass, members_password.password, members_password.salt, members_addresses.hname, members_addresses.hcity, members_addresses.hstate, members_addresses.hcode
 									from members
 									INNER JOIN members_password
@@ -39,23 +39,23 @@ function login($email, $password, $mysqli) {
         $stmt->bind_param('s', $email);  // Bind "$email" to parameter.
         $stmt->execute();    // Execute the prepared query.
         $stmt->store_result();
- 
+
         // get variables from result.
         $stmt->bind_result($user_id, $email, $username, $fname, $lname, $sClass, $db_password, $salt, $hname, $hcity, $hstate, $hcode);
         $stmt->fetch();
-		
+
 		//Assign userID to from Username in DB.
 		//$user_id = $username;
- 
+
         // hash the password with the unique salt.
         $password = hash('sha512', $password . $salt);
-		
+
         if ($stmt->num_rows == 1) {
             // If the user exists we check if the account is locked
-            // from too many login attempts 
- 
+            // from too many login attempts
+
             if (checkbrute($email, $mysqli) == true) {
-                // Account is locked 
+                // Account is locked
                 // Send an email to user saying their account is locked
                 return false;
             } else {
@@ -69,8 +69,8 @@ function login($email, $password, $mysqli) {
                     $user_id = preg_replace("/[^0-9]+/", "", $user_id);
                     $_SESSION['user_id'] = $user_id;
                     // XSS protection as we might print this value
-                    $username = preg_replace("/[^a-zA-Z0-9_\-]+/", 
-                                                                "", 
+                    $username = preg_replace("/[^a-zA-Z0-9_\-]+/",
+                                                                "",
                                                                 $username);
                     $_SESSION['username'] = $username;
 					$_SESSION['fname'] = $fname;
@@ -81,8 +81,8 @@ function login($email, $password, $mysqli) {
 					$_SESSION['hcity'] = $hcity;
 					$_SESSION['hstate'] = $hstate;
 					$_SESSION['hcode'] = $hcode;
-					
-					
+
+
 					//$_SESSION['sClass'] = $sClass;
                     $_SESSION['login_string'] = hash('sha512', $password . $user_browser);
 					//alert ('<br> $password    : ' . $password . '<br>');
@@ -107,23 +107,23 @@ function login($email, $password, $mysqli) {
 }
 
 function checkbrute($email, $mysqli) {
-    // Get timestamp of current time 
+    // Get timestamp of current time
     $now = time();
- 
-    // All login attempts are counted from the past 2 hours. 
+
+    // All login attempts are counted from the past 2 hours.
     $valid_attempts = $now - (2 * 60 * 60);
- 
-    if ($stmt = $mysqli->prepare("SELECT time 
-                             FROM login_attempts 
-                             WHERE email = ? 
+
+    if ($stmt = $mysqli->prepare("SELECT time
+                             FROM login_attempts
+                             WHERE email = ?
                             AND time > '$valid_attempts'")) {
         $stmt->bind_param('i', $email);
- 
-        // Execute the prepared query. 
+
+        // Execute the prepared query.
         $stmt->execute();
         $stmt->store_result();
- 
-        // If there have been more than 5 failed logins 
+
+        // If there have been more than 5 failed logins
         if ($stmt->num_rows > 5) {
             return true;
         } else {
@@ -134,88 +134,88 @@ function checkbrute($email, $mysqli) {
 
 function login_check($mysqli) {
     // Check if all session variables are set
-    if (isset($_SESSION['user_id'], 
-                        $_SESSION['username'], 
+    if (isset($_SESSION['user_id'],
+                        $_SESSION['username'],
                         $_SESSION['login_string'])) {
- 
+
         $user_id = $_SESSION['user_id'];
         $login_string = $_SESSION['login_string'];
         $username = $_SESSION['username'];
- 
+
         // Get the user-agent string of the user.
         $user_browser = $_SERVER['HTTP_USER_AGENT'];
- 
-        if ($stmt = $mysqli->prepare("SELECT password 
-                                      FROM members_password 
+
+        if ($stmt = $mysqli->prepare("SELECT password
+                                      FROM members_password
                                       WHERE username = ? LIMIT 1")) {
             // Bind "$user_id" to parameter.
-            $stmt->bind_param('i', $username);
+            $stmt->bind_param('s', $username);
             $stmt->execute();   // Execute the prepared query.
             $stmt->store_result();
- 
+
             if ($stmt->num_rows == 1) {
                 // If the user exists get variables from result.
                 $stmt->bind_result($password);
                 $stmt->fetch();
                 $login_check = hash('sha512', $password . $user_browser);
 				//$login_check1 = hash('sha512', $password . $user_browser);
- 
+
                if ($login_check == $login_string) {
-                    // Logged In!!!! 
+                    // Logged In!!!!
                     return true;
                 } else {
-                    // Not logged in 
+                    // Not logged in
 					echo ('Fail1');
 					echo ('<br> LoginCheck   : ' . $login_check . '<br>');
 					echo ('<br> $login_string: ' . $login_string . '<br>');
 					echo ('<br> $password  2  : ' . $password . '<br>');
 					echo ('<br> $user_browser:' . $user_browser . '<br><br>');
-					
+
 					echo ('<br> Username  : ' . $username . '<br>');
 					echo ('<br> First Name:' . $fname . '<br><br>');
-					
+
                     return false;
                 }
             } else {
-                // Not logged in 
+                // Not logged in
 				echo ('Fail2');
                 return false;
             }
         } else {
-            // Not logged in 
+            // Not logged in
 			echo ('Fail3');
             return false;
         }
     } else {
-        // Not logged in 
+        // Not logged in
 		echo ('Fail4');
         return false;
     }
 }
 
 function esc_url($url) {
- 
+
     if ('' == $url) {
         return $url;
     }
- 
+
     $url = preg_replace('|[^a-z0-9-~+_.?#=!&;,/:%@$\|*\'()\\x80-\\xff]|i', '', $url);
- 
+
     $strip = array('%0d', '%0a', '%0D', '%0A');
     $url = (string) $url;
- 
+
     $count = 1;
     while ($count) {
         $url = str_replace($strip, '', $url, $count);
     }
- 
+
     $url = str_replace(';//', '://', $url);
- 
+
     $url = htmlentities($url);
- 
+
     $url = str_replace('&amp;', '&#038;', $url);
     $url = str_replace("'", '&#039;', $url);
- 
+
     if ($url[0] !== '/') {
         // We're only interested in relative links from $_SERVER['PHP_SELF']
         return '';
