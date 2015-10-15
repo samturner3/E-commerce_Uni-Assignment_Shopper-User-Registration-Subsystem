41b6d8ace68e0ccc35aaba7bf3242818745f15fa
Jacob Williams <jacob_luke500@hotmail.com>
2015-10-15 22:25:44 +1100

    first commit
    Didnt change anything

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * These are the database login details
- */  
+ */
 define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "user1");    // The database username. 
-define("PASSWORD", '');    // The database password. 
+define("USER", "user1");    // The database username.
+define("PASSWORD", '');    // The database password.
 define("DATABASE", "NewStarPS1");    // The database name.
- 
+
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
- 
+
 define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
 ?>
