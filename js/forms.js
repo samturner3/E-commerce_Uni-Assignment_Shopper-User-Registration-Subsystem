615d3b2fffe6857eed354044a5e6f927918335b8
Jacob Williams <jacob_luke500@hotmail.com>
2015-10-21 20:42:46 +1100

    added super helpful comment

--- js/forms.js
+++ js/forms.js
@@ -275,7 +275,7 @@ else {
     }
  }
 
-    // Create a new element input, this will be our hashed password field.
+    //double hashing not needed
     /*var p = document.createElement("input");
 
     // Add the new element to our form.
